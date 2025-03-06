6d5c29d9fa8cef497901ad5c688b71e8a72b01f8
Cassidy Ho <cassidyleho@gmail.com>
2025-03-06 12:48:19 -0800

    css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ChatbotComponent } from './chatbot/chatbot.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'chatbot', component: ChatbotComponent },
+  { path: 'resources', component: ResourcesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' } // Redirect to Home
+];
