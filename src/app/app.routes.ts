021d831c3cd245824cbcd6255d97a34518b6bb69
Cassidy Ho <cassidyleho@gmail.com>
2025-03-06 13:51:53 -0800

    path fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,32 @@ import { ResourcesComponent } from './resources/resources.component';
 import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+//   { path: 'home', component: HomeComponent },
   { path: 'chatbot', component: ChatbotComponent },
   { path: 'resources', component: ResourcesComponent },
   { path: 'about', component: AboutComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' } // Redirect to Home
+  { path: ' ', component: HomeComponent }
+  //   { path: '', redirectTo: '/home', pathMatch: 'full' } 
 ];
+
+
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { HomeComponent } from './home/home.component';
+// import { ChatbotComponent } from './chatbot/chatbot.component';
+// import { ResourcesComponent } from './resources/resources.component';
+// import { AboutComponent } from './about/about.component';
+
+// const routes: Routes = [
+//   { path: ' ', component: HomeComponent },
+//   { path: 'chatbot', component: ChatbotComponent },
+//   { path: 'resources', component: ResourcesComponent },
+//   { path: 'about', component: AboutComponent },
+//   { path: '', redirectTo: '/home', pathMatch: 'full' } // Redirect to home by default
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
